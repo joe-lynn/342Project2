cc1940b55e0a97400ea1107de36ad80b3c1af282
Matthew Mansor <matthewmansor@gmail.com>
2016-12-05 10:32:36 -0500

    Wrote up Jail Actor, still needs end of day printing.

---
+++ src/Jail.java
@@ -0,0 +1,27 @@
+
+
+import akka.actor.UntypedActor;
+import akka.actor.Actors;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+* Jail class for holding passengers who fail the security check.
+* Maintains a list of passengers passed to it.
+*/
+public class Jail extends UntypedActor {
+
+  private List<Passenger> jailed = new ArrayList<Passenger>();
+  
+  public void onReceive(Object message) {
+    if(message instanceof Passenger) {
+      onReceive((Passenger)message);
+    }
+  }
+
+  private void onReceive(Passenger passenger) {
+    System.out.println(passenger.getName + " has entered the jail.");
+    jailed.add(passenger);
+  }
+}
