ac50a68e69b299fea8ecca32dfef9f60027e94d4
Owen <Owenqd@Live.com>
2016-12-05 23:26:30 -0500

    Forgot to add this in the last commit.
    This prints out passengers that pass the document check.

---
+++ src/DocumentCheck.java
@@ -0,0 +1,40 @@
+
+
+import akka.actor.UntypedActor;
+import akka.actor.ActorRef;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Actor for the Body Scanner.
+ * Code is pretty much the same as the Baggage Scanner.
+ */
+public class DocumentCheck extends UntypedActor {
+
+    private final ArrayList<ScanQueue> queueList; ;
+    private int lastQueue = -1;
+
+
+    public DocumentCheck(ArrayList<ScanQueue> queues) {
+        queueList = queues;
+    }
+
+    public void onReceive(Object message) {
+        if(message instanceof Passenger) {
+            onReceive((Passenger)message);
+        }
+    }
+
+    public void onReceive(Passenger passenger) {
+
+        if(Math.random()*5 < 4){
+            lastQueue += 1;
+            if (lastQueue == queueList.size()) {
+                lastQueue = 0;
+            }
+            //This is a test to see if actors are successfully being passed.
+            System.out.println(passenger.getName());
+        }
+    }
+}
