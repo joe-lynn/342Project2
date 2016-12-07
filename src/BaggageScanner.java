69ca3e0e49ab49baa400b699c31a4cc324b4ec3c
Matthew Mansor <matthewmansor@gmail.com>
2016-12-07 10:52:43 -0500

    Made the scanners tell the line when they're ready.

--- src/BaggageScanner.java
+++ src/BaggageScanner.java
@@ -13,6 +13,9 @@ public class BaggageScanner extends UntypedActor {
   private final int lineNumber;
   private final ActorRef station;
 
+  //Saves us from needing to recreate the message every time
+  private final BagReady READY = new BagReady();
+
   public BaggageScanner(int line, ActorRef securityStation) {
     lineNumber = line;
     station = securityStation;
@@ -28,5 +31,8 @@ public class BaggageScanner extends UntypedActor {
     //Check whether the passenger passes the security check.
     ScanReport results = new ScanReport(passenger, (Math.random()*5 < 1));
     station.tell(results,getSelf());
+
+    //Let the line know that the scanner is ready.
+    getSender().tell(READY, getSelf());
   }
 }
--- src/BodyScanner.java
+++ src/BodyScanner.java
@@ -12,6 +12,9 @@ public class BodyScanner extends UntypedActor {
   private final int lineNumber;
   private final ActorRef station;
 
+  //Saves us from needing to recreate the message every time
+  private final BodyReady READY = new BodyReady();
+
   public BodyScanner(int line, ActorRef securityStation) {
     lineNumber = line;
     station = securityStation;
@@ -27,5 +30,8 @@ public class BodyScanner extends UntypedActor {
     //Check whether the passenger passes the security check.
     ScanReport results = new ScanReport(passenger, (Math.random()*5 < 1));
     station.tell(results, getSelf());
+
+    //Let the line know that the scanner is ready.
+    getSender().tell(READY, getSelf());
   }
 }
